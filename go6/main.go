eebc9153efbcb5461b7b5e8a1a9be2209f0ef92c
zare926 <ilove.boc926@gmail.com>
2021-12-15 15:51:05 +0900

    クロージャー

---
+++ go6/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func Later() func(string) string {
+	var store string
+	return func(next string) string {
+		s := store
+		store = next
+		return s
+	}
+}
+
+func main() {
+	f := Later()
+	fmt.Println(f("Hello"))
+	fmt.Println(f("My"))
+	fmt.Println(f("name"))
+	fmt.Println(f("is"))
+	fmt.Println(f("Golang"))
+}
