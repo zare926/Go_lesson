9dd10822c779ae433c64fe01cf6351eefb3e7689
zare926 <ilove.boc926@gmail.com>
2021-12-18 16:09:53 +0900

    chanel close

---
+++ go18/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int, 2)
+	// ch1 <- 1
+
+	// close(ch1)
+	// // ch1 <-  1
+	// i, ok := <-ch1
+	// fmt.Println(i, ok)
+	// i2, ok := <-ch1
+	// fmt.Println(i2, ok)
+
+	go reciever("1.goroutin", ch1)
+	go reciever("2.goroutin", ch1)
+	go reciever("3.goroutin", ch1)
+
+	i := 0
+	for i < 100 {
+		ch1 <- i
+		i++
+	}
+	close(ch1)
+	time.Sleep(3 * time.Second)
+}
+
+func reciever(name string, ch chan int) {
+	for {
+		i, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Println(name, i)
+	}
+	fmt.Println(name, "END")
+}
