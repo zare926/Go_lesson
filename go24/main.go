beea9a4111b3dfaaf33f4a517b8d13ab182ca6ed
zare926 <ilove.boc926@gmail.com>
2021-12-19 07:14:50 +0900

    struct slice

---
+++ go24/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type Users []*User
+
+func main() {
+	// user1 := NewUser("user", 1)
+	// fmt.Println(user1)
+	// fmt.Println(*user1)
+
+	user1 := User{Name: "user1", Age: 10}
+	user2 := User{Name: "user2", Age: 20}
+	user3 := User{Name: "user3", Age: 30}
+	user4 := User{Name: "user4", Age: 40}
+
+	users := Users{}
+
+	users = append(users, &user1)
+	users = append(users, &user2)
+	users = append(users, &user3, &user4)
+
+	for _, u := range users {
+		fmt.Println(*u)
+	}
+
+	users2 := make([]*User, 0)
+	users2 = append(users2, &user1, &user2)
+
+	for _, u := range users2 {
+		fmt.Println(*u)
+	}
+}
+
+func NewUser(name string, age int) *User {
+	return &User{Name: name, Age: age}
+}
