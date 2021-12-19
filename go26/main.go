824fc32e360f2f682c856fa8bd7988faa4b0751b
zare926 <ilove.boc926@gmail.com>
2021-12-20 07:04:57 +0900

    intercase異なる型

---
+++ go26/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Stringfy interface {
+	ToString() string
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) ToString() string {
+	return fmt.Sprintf("Name=%v, Age=%v", p.Name, p.Age)
+}
+
+type Car struct {
+	Number string
+	Model  string
+}
+
+func (c *Car) ToString() string {
+	return fmt.Sprintf("Number=%v, Model=%v", c.Number, c.Model)
+}
+
+func main() {
+	vs := []Stringfy{
+		&Person{Name: "Taro", Age: 21},
+		&Car{Number: "123-456", Model: "AB-1234"},
+	}
+
+	fmt.Println(vs)
+	for _, v := range vs {
+		fmt.Println(v.ToString())
+	}
+}
