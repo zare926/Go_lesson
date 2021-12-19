3e0f03794f2985c61b5184a41fc06c3012a01480
zare926 <ilove.boc926@gmail.com>
2021-12-20 07:24:39 +0900

    customError

---
+++ go27/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type MyError struct {
+	Message string
+	ErrCode int
+}
+
+func (e *MyError) Error() string {
+	return e.Message
+}
+
+func RaiseError() error {
+	return &MyError{Message: "カスタムエラーが発生しました", ErrCode: 1234}
+}
+
+func main() {
+	err := RaiseError()
+	fmt.Println(err.Error())
+
+	e, ok := err.(*MyError)
+	if ok {
+		fmt.Println(e.ErrCode)
+	}
+}
