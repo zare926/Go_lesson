ae1ef434998e7d59931ac774637a9fe724fe08e8
zare926 <ilove.boc926@gmail.com>
2022-01-05 08:16:48 +0900

    psql操作方法、goiniの使用方法

---
+++ go37/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
+type ConfigList struct {
+	Port      int
+	DbName    string
+	SQLDriver string
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, _ := ini.Load("config.ini")
+
+	Config = ConfigList{
+		Port: cfg.Section("web").Key("port").MustInt(8080),
+
+		DbName: cfg.Section("db").Key("name").MustString("example.sql"),
+
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+	}
+}
+
+func main() {
+	fmt.Printf("Port = %v\n", Config.Port)
+	fmt.Printf("DbName = %v\n", Config.DbName)
+	fmt.Printf("SQLDriver = %v\n", Config.SQLDriver)
+}
